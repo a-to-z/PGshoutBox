0c83bd438274e0de08fcd2d8217967f679d9a427
Ammar <ammarzubair@gmail.com>
2015-05-06 18:30:43 +0500

    jsoup failed

--- app/src/main/java/com/pakgamers/pgshoutbox/MainActivity.java
+++ app/src/main/java/com/pakgamers/pgshoutbox/MainActivity.java
@@ -1,17 +1,73 @@
 package com.pakgamers.pgshoutbox;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    //URL address
+    String url = "http://www.pakgamers.com/forums/vbshout.php?do=detach&instanceid=1";
+    ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Description().execute();
+    }
+
+    // Description AsyncTask
+    private class Description extends AsyncTask<Void, Void, Void> {
+        String desc;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            mProgressDialog.setTitle("Loading ShoutBox");
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                // Connect to the web site
+                Document document = Jsoup.connect(url).userAgent("Chrome/41.0.2272.118").get();
+                // Using Elements to get the Meta data
+                Elements description = document.select("script[name=dbtech_vbshout_shouttype_shout]");
+                // Locate the content attribute
+                for (Element e : description) {
+                    desc = e.attr("name");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            // Set description into TextView
+            TextView txtdesc = (TextView) findViewById(R.id.txtView);
+            txtdesc.setText(desc);
+            mProgressDialog.dismiss();
+        }
     }
 
 
